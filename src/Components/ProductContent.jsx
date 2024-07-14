1b48ddc2b391cee46243e97d9e9d13f5344ec839
Amira Claire <107073005+claireamira@users.noreply.github.com>
2024-07-14 11:00:45 +0300

    Update ProductContent.jsx

--- src/Components/ProductContent.jsx
+++ src/Components/ProductContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from './Components/Navbar';
+import Navbar from 'src/Components/Navbar';
 import { useLocation } from 'react-router-dom';
 
 const ProductContent = () => {
